36ca4525b08c6cacf299deb820140ec658faf996
l4b4r4b4b4 <l4b4r4b4b4>
2024-02-11 19:44:55 +0000

    almost benchmark

--- api/src/rmt_laser_benchmark.py
+++ api/src/rmt_laser_benchmark.py
@@ -4,22 +4,45 @@ from src.utils import gptq_data_utils
 from tqdm import tqdm
 import numpy as np
 import gc
+from icecream import ic
+
+from src.utils.load_benchmark_dataset import get_benchmark_data
+
+from src.utils.assets import PromptTemplate
+from src.utils.prompt_template import get_llm_prompt
+import torch.nn.functional as F
+
+from src.AutoModelForSentenceEmbedding import (
+    AutoModelForSentenceEmbedding,
+    get_loss,
+    get_cosine_embeddings,
+)
+import time
 
 class ModelModifier:
     def __init__(
         self,
         model_name,
-        seqlen=64,
+        prompt_template: PromptTemplate = PromptTemplate.chatml,
+        input_length=1024,
+        output_length=1024,
     ):
         self.model_name = model_name
+        self.prompt_template = prompt_template
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name, torch_dtype=torch.bfloat16, device_map={"": 0}
         )
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name, torch_dtype=torch.bfloat16, use_fast=True
+        )
         self.layer_snr = {}
         self.modified_layers = set()
         self.original_weights = {}
-        self.seqlen = seqlen
+        self.input_length = input_length
+        self.output_length = output_length
+        self.embeddings_model = AutoModelForSentenceEmbedding(
+            model_name, self.tokenizer
+        )
 
     def calculate_snr_for_layer(self, layer_type, layer_number):
         for name, module in self.model.named_modules():
@@ -34,12 +57,11 @@ class ModelModifier:
 
                 signal = S[S > mp_threshold].sum()
                 noise = S[S <= mp_threshold].sum()
-                snr = signal / noise if noise != 0 else float('inf')
+                snr = signal / noise if noise != 0 else float("inf")
                 del S, weights
                 torch.cuda.empty_cache()  # Clear PyTorch's CUDA memory cache
                 gc.collect()
                 return snr
-        
 
     def update_model_reduce_layer(self, layer_type, layer_number):
         layer_id = f"{layer_type}+{layer_number}"
@@ -60,7 +82,9 @@ class ModelModifier:
 
                 # Calculate Marchenko-Pastur threshold
                 n, m = weights.shape
-                mp_threshold_full_iqr = self.marchenko_pastur_threshold(sigma_estimated_full_iqr, n, m)
+                mp_threshold_full_iqr = self.marchenko_pastur_threshold(
+                    sigma_estimated_full_iqr, n, m
+                )
 
                 # Retain only the singular values above the MP threshold
                 S_reduced = torch.zeros_like(S)
@@ -78,9 +102,9 @@ class ModelModifier:
     @staticmethod
     def marchenko_pastur_threshold(sigma, n, m):
         beta = n / m if n < m else m / n
-        threshold = sigma * np.sqrt((1 + np.sqrt(beta))**2)
+        threshold = sigma * np.sqrt((1 + np.sqrt(beta)) ** 2)
         return threshold
-    
+
     ## Calculate an estimate of the standard deviation of the singular values based on Inter Quantile Range
 
     @staticmethod
@@ -88,9 +112,10 @@ class ModelModifier:
         q75 = torch.quantile(S, 0.75)
         q25 = torch.quantile(S, 0.25)
         iqr = q75 - q25
-        sigma_estimated = iqr / 1.349 ## 0.6745 * sigma is the expected range between the quantiles (Q1 and Q3)
+        sigma_estimated = (
+            iqr / 1.349
+        )  ## 0.6745 * sigma is the expected range between the quantiles (Q1 and Q3)
         return sigma_estimated
-
 
     def restore_model_original_layer(self, layer_type, layer_number):
         layer_id = f"{layer_type}+{layer_number}"
@@ -106,28 +131,12 @@ class ModelModifier:
                     print(f"No original weights saved for layer: {name}", flush=True)
         return
 
-    def calculate_cosine_similarity(self, expected_answer, model_response):
-        # Tokenize the texts
-        expected_answer_tokens = self.tokenizer(expected_answer, return_tensors='pt')
-        model_response_tokens = self.tokenizer(model_response, return_tensors='pt')
+    def calculate_cosine_similarity(self, embeddings_vec_1, embeddings_vec_2):
 
-        # Get the embeddings
-        with torch.no_grad():
-            expected_answer_embeddings = self.model(**expected_answer_tokens).last_hidden_state
-            model_response_embeddings = self.model(**model_response_tokens).last_hidden_state
+        return F.cosine_similarity(embeddings_vec_1, embeddings_vec_2, dim=1)
 
-        # Calculate the cosine similarity
-        cosine_similarity = torch.nn.functional.cosine_similarity(
-            expected_answer_embeddings.mean(dim=1),
-            model_response_embeddings.mean(dim=1)
-        )
-
-        return cosine_similarity.item()
-
-    def calculate_perplexity(self, prompt_input, expected_answer):
+    def calculate_perplexity(self, inputs, labels):
         # Encode the input and expected answer
-        inputs = self.tokenizer.encode(prompt_input, return_tensors='pt')
-        labels = self.tokenizer.encode(expected_answer, return_tensors='pt')
 
         # Calculate the loss
         with torch.no_grad():
@@ -139,62 +148,157 @@ class ModelModifier:
 
         return perplexity.item()
 
-    def calculate_model_performance(self, inputs, expected_answers):
+    def calculate_model_performance(
+        self,
+        datasets=["orca_dpo", "ultrafeedback"],  # "openhermes"
+        n_samples=256,
+        input_length=1024,
+        output_length=1024,
+    ):
         performance_scores = []
-        perplexities = []
+        rejection_scores = []
+        loss_accum = 0.0
+        loss_fct = torch.nn.CrossEntropyLoss().cuda()
+        model = self.model
+        tokenizer = self.tokenizer
+        embeddings_model = self.embeddings_model
+        model.to("cuda")
+        for dataset in datasets:
+            benchmark_dataset = get_benchmark_data(
+                dataset, n_samples, input_length, output_length
+            )
 
-        for prompt_input, expected_answer in zip(inputs, expected_answers):
-            # Generate the model's output
-            model_output = self.model.generate(prompt_input)
+            for sample in benchmark_dataset.data:
+                prompt = get_llm_prompt(sample.instruction, sample.prompt)
+                prompt_enc = tokenizer([prompt], return_tensors="pt")
+                prompt_enc.to("cuda")
+                expected_answer = sample.chosen
+                expected_answer_enc = tokenizer(
+                    [expected_answer],
+                    return_tensors="pt",
+                    padding="max_length",
+                    max_length=self.output_length,
+                )
+                expected_answer_enc.to("cuda")
+                expected_answer_embs = embeddings_model(**expected_answer_enc)
+                # ic(expected_answer_embs)
+                rejected_answer = sample.rejected
+                rejected_answer_enc = tokenizer(
+                    [rejected_answer],
+                    return_tensors="pt",
+                    padding="max_length",
+                    max_length=self.output_length,
+                )
+                rejected_answer_enc.to("cuda")
+                rejected_answer_embs = embeddings_model(**rejected_answer_enc)
 
-            # Calculate the cosine similarity and perplexity
-            score = self.calculate_cosine_similarity(model_output, expected_answer)
-            perplexity = self.calculate_perplexity(prompt_input, expected_answer)
+                # Generate the model's output
+                # model_output = model(
+                #     prompt_enc.input_ids,
+                #     use_cache=False,
+                #     output_hidden_states=False,
+                #     output_attentions=False,
+                # )[0]
+                model_output = model.generate(
+                    **prompt_enc,
+                    max_new_tokens=self.output_length,
+                    use_cache=False,
+                    output_hidden_states=False,
+                    output_attentions=False,
+                )
+                ic(model_output)
+                output_ids = model_output[0].argmax(dim=-1)
+                output_string = tokenizer.decode(output_ids, skip_special_tokens=True)
+                ic(output_string)
+                time.sleep(60)
+                model_output_embs = embeddings_model(**model_output)
+                cosine_similarity_gain = get_cosine_embeddings(
+                    model_output_embs, expected_answer_embs
+                )
+                cosine_similarity_loss = get_cosine_embeddings(
+                    model_output_embs, rejected_answer_embs
+                )
+                ic(cosine_similarity_gain, cosine_similarity_loss)
+                # rejection_score = self.calculate_cosine_similarity(
+                #     rejected_answer_enc, model_output
+                # )
+                # performance_scores.append(score)
+                # rejection_scores.append(rejection_score)
+                # Calculate the perplexity
+                # loss_accum += 1
+                # loss_accum += loss_fct(model_output, expected_answer_enc)
+                # model_output.to("cuda")
 
-            performance_scores.append(score)
-            perplexities.append(perplexity)
+                # ic(model_output)
+                # # expected_answer_enc.cuda()
+                # ic(type(expected_answer_enc), expected_answer_enc)
+                # loss = loss_fct(model_output, expected_answer_enc.input_ids)
+                # loss_accum += loss.item()
 
-        return performance_scores, perplexities
-    
+        avg_loss = loss_accum / (n_samples * len(datasets))
+        average_perplexity = torch.exp(torch.tensor(avg_loss)).item()
+        performance = -average_perplexity
+        return performance
+
     def assess_layers_snr(self, layer_types, layer_numbers):
         for name, _ in self.model.named_modules():
             for layer_number in layer_numbers:
                 for layer_type in layer_types:
                     if layer_type in name and str(layer_number) in name:
                         layer_name = f"{layer_type}+{layer_number}"
-                        print("*"*50, flush=True)
-                        print(f"Calculating Signal to Noise Ratio at layer {layer_name}", flush=True)
+                        print("*" * 50, flush=True)
+                        print(
+                            f"Calculating Signal to Noise Ratio at layer {layer_name}",
+                            flush=True,
+                        )
                         snr = self.calculate_snr_for_layer(layer_type, layer_number)
                         self.layer_snr[layer_name] = snr
-                        print(f"Signal to Noise Ratio at layer {layer_name} = {snr}", flush=True)
-                        print("*"*50, flush=True)
+                        print(
+                            f"Signal to Noise Ratio at layer {layer_name} = {snr}",
+                            flush=True,
+                        )
+                        print("*" * 50, flush=True)
 
     def select_layers_for_modification(self, k):
-        sorted_layers = sorted(self.layer_snr.items(), key=lambda x: x[1], reverse=False)
+        sorted_layers = sorted(
+            self.layer_snr.items(), key=lambda x: x[1], reverse=False
+        )
         return [layer[0] for layer in sorted_layers[:k]]
-    
+
     def test_and_modify_layers(self, candidate_layers):
-        initial_perplexity = self.calculate_model_performance(datasets=['wikitext2', 'ptb', 'c4'], seqlen=128)
-        print(f"Initial Model Perplexity: {initial_perplexity}")
+        initial_performance = self.calculate_model_performance()
+
+        print(f"Initial Model Performance: {initial_performance}")
 
         for layer in candidate_layers:
             # Modify the layer
             layer_type = layer.split("+")[0]
             layer_number = layer.split("+")[1]
-            self.update_model_reduce_layer(layer_type=layer_type,layer_number=layer_number)
-            
+            self.update_model_reduce_layer(
+                layer_type=layer_type, layer_number=layer_number
+            )
+
             # Test the model's performance
-            new_perplexity = self.calculate_model_performance(datasets=['wikitext2', 'ptb', 'c4'], seqlen=128)
-            print(f"Tested Model Perplexity after modifying {layer}: {new_perplexity}")
+            new_performance = self.calculate_model_performance()
+            print(
+                f"Tested Model Performance after modifying {layer}: {new_performance}"
+            )
 
             # If the perplexity does not improve significantly, revert the change
-            if new_perplexity > initial_perplexity:
-                self.restore_model_original_layer(layer_type=layer_type,layer_number=layer_number)
-                print(f"Reverted changes in {layer} due to lack of improvement.", flush=True)
+            if new_performance <= initial_performance:
+                self.restore_model_original_layer(
+                    layer_type=layer_type, layer_number=layer_number
+                )
+                print(
+                    f"Reverted changes in {layer} due to lack of improvement.",
+                    flush=True,
+                )
             else:
-                initial_perplexity = new_perplexity
-                print(f"Modification kept for {layer}. New baseline perplexity: {initial_perplexity}", flush=True)
-
+                initial_performance = new_performance
+                print(
+                    f"Modification kept for {layer}. New baseline perplexity: {initial_performance}",
+                    flush=True,
+                )
 
     def save_model(self, save_dir):
 
